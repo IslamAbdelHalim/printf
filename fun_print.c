8c5dfcbd2b434649b9b50cc021aa3dad8ad9057f
IslamAhmed <eslamelbeltagye1111@gmail.com>
2023-09-20 20:35:08 +0300

    a

--- fun_print.c
+++ fun_print.c
@@ -46,3 +46,58 @@ int print_char(va_list arg)
 	_putchar(ch);
 	return (1);
 }
+
+/**
+ * print_integer - Prints an integer
+ * @arg: The argument
+ *
+ * Return: The number of characters printed.
+ */
+
+int print_integer(va_list arg)
+{
+	int num, digits = 0, temp, divisor, i, digit, count;
+
+	num = va_arg(arg, int);
+
+	if (num < 0)
+	{
+		_putchar('-');
+		num *= -1;
+	}
+
+	temp = num;
+
+	while (temp != 0)
+	{
+		temp /= 10;
+		digits++;
+	}
+
+	count = digits;
+
+	while (digits > 0)
+	{
+		divisor = 1;
+		for (i = 1; i < digits; i++)
+			divisor *= 10;
+		digit = num / divisor;
+		_putchar(digit + '0');
+		num %= divisor;
+		digits--;
+	}
+	return (count);
+}
+
+/**
+ * print_decimal - function to handl %d
+ *
+ * @arg: The argument
+ *
+ * Return: The count of number
+*/
+
+int print_decimal(va_list arg)
+{
+	return (print_integer(arg));
+}
